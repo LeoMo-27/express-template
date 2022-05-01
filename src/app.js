e5764ce6ade250bbe8625c2fe82d91c5db56e54b
José Manuel Madriaza Caravia <jm.madriaza@uc.cl>
2022-04-30 22:36:09 -0400

    chore: add cookie-parser

--- src/app.js
+++ src/app.js
@@ -4,13 +4,17 @@ require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const routes = require('./routes');
 
 const app = express();
-app.use(cors());
 
+app.use(cors());
 app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(routes);
 
 app.listen(process.env.PORT || 3000, () => {
