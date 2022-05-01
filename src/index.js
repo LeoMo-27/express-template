f771c8af3ea50a57606aaa40efe9712f300880cc
José Manuel Madriaza Caravia <jm.madriaza@uc.cl>
2022-04-30 20:04:54 -0400

    feat: add hello world express app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+  
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
