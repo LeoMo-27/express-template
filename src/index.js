8c3178ca0538ebf788c9801c6c91108a7ff16587
José Manuel Madriaza Caravia <jm.madriaza@uc.cl>
2022-04-30 20:06:33 -0400

    fix: fix port variabel

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
-  
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
 })
