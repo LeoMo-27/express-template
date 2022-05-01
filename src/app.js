f08427746d2b50e67e34e82f214aeea8432a86f6
José Manuel Madriaza Caravia <jm.madriaza@uc.cl>
2022-04-30 22:33:24 -0400

    chore: add cors

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ require('dotenv').config();
 
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 
 const routes = require('./routes');
 
 const app = express();
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(routes);
